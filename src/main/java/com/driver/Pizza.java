76ba3ca360b6d676bbd5eb4c9496e39334ea0b41
MuthuGurumorthy <88612046+MuthuGurumorthy@users.noreply.github.com>
2022-11-30 19:50:33 +0530

    new

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,8 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.price+=(isVeg?300:400);
+        System.out.println("Base Price Of The Pizza: "+(isVeg?"300":"400"));
     }
 
     public int getPrice(){
@@ -17,18 +19,25 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.price+=80;
+        System.out.println("Extra Cheese Added: 80");
     }
 
     public void addExtraToppings(){
         // your code goes here
+        this.price+=isVeg?70:120;
+        System.out.println("Extra Toppings Added: "+(isVeg?"70":"120"));
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price+=20;
+        System.out.println("Paperbag Added: 20");
     }
 
     public String getBill(){
         // your code goes here
+        System.out.println("Total Price:"+this.price);
         return this.bill;
     }
 }
